b337bc1ce083b82725b0ae5e85036616cba5e7ee
Horacio Bertorello <syrii@msn.com>
2015-05-05 18:24:10 -0300

    Add Custom Checkout API support.

---
+++ lib/mercadopago/custom_checkout/payment_methods.rb
@@ -0,0 +1,20 @@
+module MercadoPago
+  module CustomCheckout
+    module PaymentMethods
+      def self.get(access_token)
+        headers = { accept: 'application/json' }
+        MercadoPago::Request.wrap_get("/v1/payment_methods?access_token=#{access_token}", headers)
+      end
+
+      def self.get_installments(access_token, payload)
+        headers = { accept: 'application/json' }
+        MercadoPago::Request.make_request(:get, "/v1/payment_methods/installments?access_token=#{access_token}", payload, headers)
+      end
+
+      def self.get_card_issuers(access_token)
+        headers = { accept: 'application/json' }
+        MercadoPago::Request.wrap_get("/v1/payment_methods/card_issuers?access_token=#{access_token}", headers)
+      end
+    end
+  end
+end
